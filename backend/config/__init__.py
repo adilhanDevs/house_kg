ba405f592f6f766de5a291773710456a2d1d20a4
ibrodevs <ibroabdraimov20@gmail.com>
2026-09-02 15:03:31 +0600

    deleted backend folder

--- backend/config/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Проект house_kgz.
-
-Импорт celery-приложения здесь гарантирует, что shared_task-декораторы
-подхватят его при старте Django.
-"""
-
-from .celery import app as celery_app
-
-__all__ = ("celery_app",)
